e64665caef3624237f6e16b638e7d12cd36f8fd2
msosa <uy.msosa@gmail.com>
2023-08-11 16:42:58 -0300

    entrega 30%

--- Entregas/Proyecto_final/01_entrega/src/app.js
+++ Entregas/Proyecto_final/01_entrega/src/app.js
@@ -5,6 +5,7 @@ import productsRouter from "./routes/products.js";
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productsRouter)
 // app.use("/api/carts", productsCarts)
--- Entregas/Proyecto_final/01_entrega/src/routes/products.js
+++ Entregas/Proyecto_final/01_entrega/src/routes/products.js
@@ -1,30 +1,120 @@
-import {Router} from "express";
+import { Router } from "express";
+import ProductManager from "./../ProductManager.js";
 
 const router = Router();
-const users = [
-    {
-        name: "Manuel",
-        apellido: "Sosa" 
-    },
-    {
-        name: "Manuel",
-        apellido: "Sosa" 
-    },
-    {
-        name: "Manuel",
-        apellido: "Sosa" 
-    }
-];
+const productManager = new ProductManager("src/products.json");
 
 router.use((req, res, next) => {
-    console.log("working");
-    next();
-})
+  next();
+});
 
-router.get("/", (req, res) => {
-    const limit = req.query.limit;
-    console.log(limit);
-    res.send(users);
-})
+//ROOT & QUERY
+router.get("/", async (req, res) => {
+  let limit = req.query.limit;
+
+  const products = await productManager.getProducts();
+
+  if (!limit) {
+    res.send(products);
+    return;
+  }
+
+  if (isNaN(limit)) {
+    return res
+      .status(400)
+      .send({ status: "erorr", error: "limit debe ser un numero" });
+  }
+
+  const productsLimited = products.slice(0, limit);
+  return res.send(productsLimited);
+});
+
+//PARAMS
+router.get("/:pid", async (req, res) => {
+  const searchId = +req.params.pid;
+
+  if (isNaN(searchId)) {
+    return res
+      .status(400)
+      .send({ status: "error", error: "searchId debe ser un numero" });
+  }
+
+  const products = await productManager.getProducts();
+
+  if (searchId) {
+    const productById = products.find((el) => el.id === searchId);
+    if (!productById) {
+      return res
+        .status(400)
+        .send({ status: "error", error: "El producto no existe" });
+    }
+    return res.send(productById);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const {
+    title,
+    description,
+    price,
+    thumbnails,
+    stock,
+    status,
+    category,
+    code,
+  } = req.body;
+  //   const newProduct = req.body;
+  console.log(req.body);
+
+  //Chequeamos que no falten datos requeridos
+  if (
+    !title ||
+    !description ||
+    !code ||
+    !price ||
+    !status ||
+    !stock ||
+    !category
+  ) {
+    return res
+      .status(400)
+      .send({ status: "error", error: "Faltan datos obligatorios" });
+  }
+
+  //Chequeamos que
+  if (
+    typeof title != "string" ||
+    typeof description != "string" ||
+    typeof code != "string" ||
+    typeof isNaN(price) ||
+    typeof status != "boolean" ||
+    typeof isNaN(stock) ||
+    typeof category != "string"
+  ) {
+    return res
+      .status(400)
+      .send({
+        status: "error",
+        error: "Parece que algunos datos no están en un formato admitido",
+      });
+  }
+
+  await productManager.addProduct(
+    title,
+    description,
+    price,
+    thumbnails,
+    stock,
+    status,
+    category,
+    code
+  );
+
+  return res
+    .status(200)
+    .send({ status: "success", success: "Producto agregado correctamente" });
+
+  // productManager.addProduct(newProduct);
+});
 
 export default router;
